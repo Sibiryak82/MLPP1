29799ad708e3c0b86043bcfe8f4fefbe34a3786c
Sibiryak82 <joker198223@gmail.com>
2021-08-16 07:54:12 +0800

    3

--- 1.11.make_db_shelve.py
+++ 1.11.make_db_shelve.py
@@ -1,5 +1,13 @@
 # Пример 1.11.PP4E\Preview\make_db_shelve.py
 
+"""Этот сценарий создаст в текущем каталоге один или более файлов, имена которых начинаются с префикса people-shelve (в ОС Windows, под
+управлением Python 3.1, сценарий создаст файлы people-shelve.bak,
+people-shelve.dat и people-shelve.dir). Вы не должны удалять эти файлы
+(они составляют вашу базу данных!), а чтобы получить доступ к этому
+хранилищу в других сценариях, необходимо использовать то же самое
+имя базы.
+"""
+
 from initdata import bob, sue
 import shelve
 db = shelve.open('people-shelve')
