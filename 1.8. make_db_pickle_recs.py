ed77851017ab5012ac8ca4d491ec7783f962ee73
Sibiryak82 <joker198223@gmail.com>
2021-08-16 08:02:45 +0800

    3

--- 1.8. make_db_pickle_recs.py
+++ 1.8. make_db_pickle_recs.py
@@ -1,5 +1,11 @@
 # Пример 1.8.PP4E\Preview\make_db_pickle_recs.py
 
+"""
+Сценарий из примера 1.8 сохраняет каждую запись в отдельном файле, где в
+качестве имени файла используется уникальный ключ записи, к которому добавляется расширение .pkl
+(он создает файлы bob.pkl, sue.pkl и tom.pkl в текущем рабочем каталоге).
+"""
+
 from initdata import bob, sue, tom
 import pickle
 for(key, record) in [('bob', bob), ('tom', tom), ('sue', sue)]:
