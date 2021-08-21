7e0e6ea4bf1a44c96ab9296a2918b60a6557308f
Алексей <joker198223@gmail.com>
2021-08-22 06:39:34 +0800

    изменил имена файлов

--- manager.py
+++ manager.py
@@ -7,9 +7,11 @@
 """
 from person import Person
 
+
 class Manager(Person):
     def giveRaise(self, percent, bonus=0.1):
         self.pay *= (1.0 + percent + bonus)
+
 
 if __name__ == '__main__':
     tom = Manager(name='Tom Doe', age=50, pay=50000)
--- person.py
+++ person.py
@@ -5,16 +5,21 @@
 и увеличения оклада в виде методов. Для доступа к обрабатываемому
 экземпляру (записи) методы используют аргумент self.
 """
+
+
 class Person:
     def __init__(self, name, age, pay=0, job=None):
         self.name = name
         self.age = age
         self.pay = pay
         self.job = job
+
     def lastName(self):
         return self.name.split()[-1]
+
     def giveRaise(self, percent):
         self.pay *= (1.0 + percent)
+
 
 if __name__ == '__main__':
     bob = Person('Bob Smith', 42, 30000, 'software')
