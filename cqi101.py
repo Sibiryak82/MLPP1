48a80326ced262604e6979f2ef7790e776c3a3b8
Алексей <joker198223@gmail.com>
2021-09-01 16:18:38 +0800

    11

---
+++ cqi101.py
@@ -0,0 +1,14 @@
+# Пример 1.30.PP4E/Preview/cqi101.html
+
+
+from lxml import html
+
+<html>
+<title>Interactive Page</title>
+<body>
+<form method=POST action="cqi-bin/cqi101.py">
+    <P><B>Enter your name:</B>
+    <P><input type=text name=user>
+    <P><input type=submit>
+</form>
+</body></html>
